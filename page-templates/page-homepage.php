d86fdb52c8268a2c1b7ed90a70cef4419e92b99a
Kyle Turco <kyleturco@gmail.com>
2016-02-06 14:45:22 -0600

    add page-templates directory, test with local wp

---
+++ page-templates/page-homepage.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content">
+  <div id="village-hero-section">
+    <h1>testing</h1>
+  </div>
+  <div id="village-exp-section">
+    <h2>testing round 2</h2>
+  </div>
+</div>
+
+<?php get_footer(); ?>
