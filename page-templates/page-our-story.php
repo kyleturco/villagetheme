5e5788ee83a7e3d26c7eb8519c50f892170c2122
Kyle Turco <kyleturco@gmail.com>
2016-02-14 17:52:27 -0600

    start story page template

--- page-templates/page-our-story.php
+++ page-templates/page-our-story.php
@@ -8,11 +8,63 @@ Template Name: Our Story
 
 <div id="content">
   <div class="container">
-    <div id="village-hero-section">
-      <h1>testing</h1>
+    <div id="story-text-section">
+      <div class="col-md-7">
+        <h1>Our Story</h1>
+        <p>
+          The Village Nursery School was established at First United Methodist Church, West Lafayette, Ind.,
+          in 1972 with the philosophy and educational understanding that young children learn best in an
+          atmosphere of purposeful and productive play experiences.
+          The learning activities planned, the selection of educational toys and materials, and even the
+          organization of the space and the schedule give children the foundation of learning they need to
+          succeed throughout their academic careers and beyond. Our programs serve children ages infant
+          through preschool.
+          <br>
+          There are changing weekly themes that give the children an exposure to many interesting science
+          and social studies related topics. In the course of their years at Village, culminating in our
+          pre-k class, the children are well prepared for entering kindergarten.
+          <br>
+          We are very proud of the success of our “alums." It is our goal that VNS children become independent,
+          self confident, curious learners who work well with others. Besides receiving an excellent pre-reading
+          and math foundation, VNS children also learn how to be a member of a group as they sing together,
+          participate in interesting discussions, take turns with the weather chart and calendar, or listen
+          to a story. They also learn to complete “assignments”, follow multi-step verbal directions, wait
+          their turn to speak, and be responsible for their school things.
+        </p>
+      </div>
+      <div id="photos-right" class="col-md-5">
+        <div class="photos-top">
+          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" alt="" width="400px">
+        </div>
+        <div class="photos-bottom">
+          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-2.jpg" alt="" width="170px">
+          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-3.jpg" alt="" width="170px">
+        </div>
+      </div>
+      <div class="clearfix"></div>
     </div>
-    <div id="village-exp-section">
-      <h2>testing round 2</h2>
+    <div id="story-director-section">
+      <h2>A Note from our Director</h2>
+      <p>
+        I have been one of the "Village" people for the last 6 years. Here at VNS I have played many roles
+        including parent, teacher, and substitute. I'm fortunate to say VNS has been my family's "home away
+        from home" for the entire time we have lived in the Lafayette area.
+        <br>
+        On a professional level I have a
+        Bachelor’s Degree in Elementary Education; I am also a licensed teacher in Indiana. I have taught
+        many of our classes at VNS, kindergarten in Indiana and fifth grade in Virginia. I took time off of
+        teaching to stay at home while my children were young.
+        <br>
+        To me, every person associated with Village
+        Nursery School, whether they are an employee, student, parent/guardian or FUMC church member, provides
+        a unique contribution to our special school.  As Director, I hope we all find our fit in our "home away
+        from home" as we work together here at Village Nursery School and FUMC.
+      </p>
+      <p>
+        Looking forward,
+      </p>
+      <h4>Deanna Bibler</h4>
+      <h5>Village Nursery School Director</h5>
     </div>
   </div>
 </div>
