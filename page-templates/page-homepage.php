301f539b4addedd5ee67e9a52f62374dae156db9
Kyle Turco <kyleturco@gmail.com>
2016-02-17 00:29:07 -0600

    update homepage template, adjust mobile style

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -24,24 +24,24 @@ Template Name: Home Page
   <div id="home-overview-section">
     <div class="container">
       <div id="inner-content">
-        <div id="content-left" class="col-md-5">
+        <div id="overview-content-left" class="col-md-6">
           <h1>Overview</h1>
-          <h4>
-            Our philosophy is learning through play as we offer a stimulating environment for children.
-          </h4>
-          <p>
-            The Village Nursery School was established at First United Methodist Church, West Lafayette, Ind., in 1972
-            with the philosophy and educational understanding that young children learn best in an atmosphere of
-            purposeful and productive play experiences.
-          </p>
-        </div>
-        <div id="photos-right" class="col-md-7">
-          <div class="photo-left" style="width: 66%; float: left; display; block">
-            <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" alt="" width="400px">
+          <div class="about-text">
+            <h4>
+              Our philosophy is learning through play as we offer a stimulating environment for children.
+            </h4>
+            <p>
+              The Village Nursery School was established at First United Methodist Church, West Lafayette, Ind., in 1972
+              with the philosophy and educational understanding that young children learn best in an atmosphere of
+              purposeful and productive play experiences.
+            </p>
           </div>
-          <div class="photos-right" style="width: 33%; float: left; display; block">
-            <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-2.jpg" alt="" width="170px">
-            <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-3.jpg" alt="" width="170px">
+        </div>
+        <div id="overview-photos-right" class="col-md-6">
+          <div class="photo-left"></div>
+          <div class="photos-right">
+            <div class="photo-1"></div>
+            <div class="photo-2"></div>
           </div>
         </div>
       </div>
@@ -50,7 +50,7 @@ Template Name: Home Page
   <div class="clearfix"></div>
   <div id="home-classes-section">
     <div class="container">
-      <h1>Our Classes</h1>
+      <h1 class="text-center">Our Classes</h1>
       <div class="class-box pull-left">
         <div class="class-box-text pull-left">
           <h2>Preschool 2</h2>
@@ -61,9 +61,7 @@ Template Name: Home Page
           </p>
           <a href="<?php echo home_url(); ?>/programs-rates/#class-1" class="btn btn-primary">Enroll Now</a>
         </div>
-        <div class="class-box-image pull-right">
-          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" width="130px">
-        </div>
+        <div class="class-box-image-1 pull-right"></div>
       </div>
       <div class="class-box pull-right">
         <div class="class-box-text pull-left">
@@ -75,13 +73,11 @@ Template Name: Home Page
           </p>
           <a href="<?php echo home_url(); ?>/programs-rates/#class-2" class="btn btn-primary">Enroll Now</a>
         </div>
-        <div class="class-box-image pull-right">
-          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" width="130px">
-        </div>
+        <div class="class-box-image-2 pull-right"></div>
       </div>
       <div class="class-box pull-left">
         <div class="class-box-text pull-left">
-          <h2>Pre-Kindergarten</h2>
+          <h2>Pre-K</h2>
           <p>
             <b>Ages:</b> 4-5
             <br>
@@ -89,9 +85,7 @@ Template Name: Home Page
           </p>
           <a href="<?php echo home_url(); ?>/programs-rates/#class-3" class="btn btn-primary">Enroll Now</a>
         </div>
-        <div class="class-box-image pull-right">
-          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" width="130px">
-        </div>
+        <div class="class-box-image-3 pull-right"></div>
       </div>
       <div class="class-box pull-right">
         <div class="class-box-text pull-left">
@@ -103,16 +97,14 @@ Template Name: Home Page
           </p>
           <a href="<?php echo home_url(); ?>/programs-rates/#class-4" class="btn btn-primary">Enroll Now</a>
         </div>
-        <div class="class-box-image pull-right">
-          <img src="<?php echo get_template_directory_uri(); ?>/library/images/home-photo-1.jpg" width="130px">
-        </div>
+      <div class="class-box-image-4 pull-right"></div>
       </div>
     </div>
   </div>
   <div id="enroll-section">
     <div class="container">
       <div class="col-md-9">
-        <h2 class="text-white">Enroll your child today!</h2>
+        <h2>Enroll your child today!</h2>
       </div>
       <div class="col-md-3">
         <button class="btn btn-primary">Learn More</button>
