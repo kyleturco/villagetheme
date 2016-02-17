4912ea6e8af085e6971ed8216fe33dd4e97fbda2
Kyle Turco <kyleturco@gmail.com>
2016-02-16 23:18:46 -0600

    add responsivity and download boxes to p&r page template

--- page-templates/page-programs-rates.php
+++ page-templates/page-programs-rates.php
@@ -13,11 +13,26 @@ Template Name: Programs & Rates
       <p id="program-subheader" class="text-center text-white">Information on our classes and offerings</p>
     </div>
   </div>
+  <div id="programs-subnav">
+    <div class="container">
+      <ul class="list-inline text-center">
+        <li><a href="#class-1">Preschool 2</a></li>
+        <li><a href="#class-2">Preschool 3</a></li>
+        <li><a href="#class-3">Pre-Kindergarten</a></li>
+        <li><a href="#class-4">Parent-Infant-Toddler</a></li>
+      </ul>
+    </div>
+  </div>
   <div id="programs-program-content">
     <div class="container">
       <div id="class-1" class="class-section">
-        <div class="class-1-header">
-          <h2>Preschool 2</h2>
+        <div class="class-1-header text-center">
+          <h1>Preschool 2</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
         </div>
         <div class="class-info">
           <div class="class-info-header">
@@ -62,8 +77,13 @@ Template Name: Programs & Rates
         </div>
       </div>
       <div id="class-2" class="class-section">
-        <div class="class-1-header">
-          <h2>Preschool 2</h2>
+        <div class="class-1-header text-center">
+          <h1>Preschool 3</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
         </div>
         <div class="class-info">
           <div class="class-info-header">
@@ -107,6 +127,109 @@ Template Name: Programs & Rates
           </p>
         </div>
       </div>
+      <div id="class-3" class="class-section">
+        <div class="class-1-header text-center">
+          <h1>Pre-Kindergarten</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
+        </div>
+        <div class="class-info">
+          <div class="class-info-header">
+            <h3><i class="fa fa-child"></i> Ages</h3>
+          </div>
+            <div class="class-info-item info-1">
+              <p>2-3 years old (must be 2 by August 1 of the school year)</p>
+            </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-usd"></i> Tuition</h3>
+          </div>
+          <div class="tuition-info">
+            <div class="class-info-item info-1">
+              <p>$50/month for 1 class/week</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>$90/month for 2 classes/week</p>
+            </div>
+          </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+          </div>
+          <div class="schedule-info">
+            <div class="class-info-item info-1">
+              <p>Monday/Wednesday 9:00-11:20 AM</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>Tuesday/Thursday 9:00-11:20 AM</p>
+            </div>
+            <div class="class-info-item info-3">
+              <p>Tuesday/Thursday 12:30-2:50 PM</p>
+            </div>
+            <div class="class-info-item info-4">
+              <p>Friday 9:00-11:20 AM</p>
+            </div>
+          </div>
+        </div>
+        <div class="class-info-notes">
+          <p>
+            *There is a maximum of 10 children with 2 teachers per class.
+          </p>
+        </div>
+      </div>
+      <div id="class-4" class="class-section">
+        <div class="class-1-header text-center">
+          <h1>Parent-Infant-Toddler</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
+        </div>
+        <div class="class-info">
+          <div class="class-info-header">
+            <h3><i class="fa fa-child"></i> Ages</h3>
+          </div>
+            <div class="class-info-item info-1">
+              <p>2-3 years old (must be 2 by August 1 of the school year)</p>
+            </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-usd"></i> Tuition</h3>
+          </div>
+          <div class="tuition-info">
+            <div class="class-info-item info-1">
+              <p>$50/month for 1 class/week</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>$90/month for 2 classes/week</p>
+            </div>
+          </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+          </div>
+          <div class="schedule-info">
+            <div class="class-info-item info-1">
+              <p>Monday/Wednesday 9:00-11:20 AM</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>Tuesday/Thursday 9:00-11:20 AM</p>
+            </div>
+            <div class="class-info-item info-3">
+              <p>Tuesday/Thursday 12:30-2:50 PM</p>
+            </div>
+            <div class="class-info-item info-4">
+              <p>Friday 9:00-11:20 AM</p>
+            </div>
+          </div>
+        </div>
+        <div class="class-info-notes">
+          <p>
+            *There is a maximum of 10 children with 2 teachers per class.
+          </p>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
