a153fda16e74b706de035baa86cfb8d94d590247
Kyle Turco <kyleturco@gmail.com>
2016-02-18 00:31:04 -0600

    add styles and formatting for enrollment template

--- page-templates/page-enrollment.php
+++ page-templates/page-enrollment.php
@@ -7,24 +7,25 @@ Template Name: Enrollment
 <?php get_header(); ?>
 
 <div id="content">
-  <div id="programs-page-header">
+  <div id="enrollment-page-header">
     <div class="container">
-      <h1 id="program-header" class="text-center text-white">Enrollment</h1>
-      <p id="program-subheader" class="text-center text-white">The info you need to get started today</p>
+      <h1 id="enrollment-header" class="text-center text-white">Enrollment</h1>
+      <p id="enrollment-subheader" class="text-center text-white">The info you need to get started today</p>
     </div>
   </div>
-  <div id="programs-subnav">
+  <div id="enrollment-subnav">
     <div class="container">
       <ul class="list-inline text-center">
-        <li><a href="#class-1">Preschool 2</a></li>
-        <li><a href="#class-2">Preschool 3</a></li>
-        <li><a href="#class-3">Pre-Kindergarten</a></li>
-        <li><a href="#class-4">Parent-Infant-Toddler</a></li>
+        <li><a href="#preschool-2">Preschool 2</a></li>
+        <li><a href="#preschool-3">Preschool 3</a></li>
+        <li><a href="#pre-k">Pre-Kindergarten</a></li>
+        <li><a href="#pit-enroll">Parent-Infant-Toddler</a></li>
+        <li><a href="#stay-and-play-enroll">Stay & Play</a></li>
       </ul>
     </div>
   </div>
-  <div id="programs-program-content">
-    <div id="class-1" class="class-section">
+  <div id="enrollment-program-content">
+    <div id="preschool-2" class="class-section class-1">
       <div class="container">
         <div class="class-1-header text-center">
           <h1>Preschool 2</h1>
@@ -77,7 +78,7 @@ Template Name: Enrollment
         </div>
       </div>
     </div>
-    <div id="class-2" class="class-section">
+    <div id="preschool-3" class="class-section class-2">
       <div class="container">
         <div class="class-1-header text-center">
           <h1>Preschool 3</h1>
@@ -128,7 +129,7 @@ Template Name: Enrollment
         </div>
       </div>
     </div>
-    <div id="class-3" class="class-section">
+    <div id="pre-k" class="class-section class-3">
       <div class="container">
         <div class="class-1-header text-center">
           <h1>Pre-Kindergarten</h1>
@@ -182,6 +183,114 @@ Template Name: Enrollment
           <p>
             **This class is for 3-5yr olds with a PreK curriculum modified to Preschool 2 for individuals who'd benefit
             from it.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div id="pit-enroll" class="class-section class-4">
+      <div class="container">
+        <div class="class-1-header text-center">
+          <h1>P.I.T.</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
+        </div>
+        <div class="class-info">
+          <div class="class-info-header">
+            <h3><i class="fa fa-child"></i> Ages</h3>
+          </div>
+            <div class="class-info-item info-1">
+              <p>4-5 years old (must be 4 by August 1 of the school year)</p>
+            </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-usd"></i> Tuition</h3>
+          </div>
+          <div class="tuition-info">
+            <div class="class-info-item info-1">
+              <p>$125/month for 1 class/week</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>$200/month for 2 classes/week</p>
+            </div>
+          </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+          </div>
+          <div class="schedule-info">
+            <div class="class-info-item info-1">
+              <p>Monday/Wednesday/Friday 9:00-11:30 AM</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>Monday-Friday 9:00-11:30 AM</p>
+            </div>
+            <div class="class-info-item info-3">
+              <p>*Tuesday/Wednesday/Thursday 12:30-3:00 PM</p>
+            </div>
+            <div class="class-info-item info-4">
+              <p>**Tuesday-Wednesday-Thursday 12:30-2:50 PM</p>
+            </div>
+          </div>
+        </div>
+        <div class="class-info-notes">
+          <p>
+            *This class is for 3-5yr olds with a Pre-K curriculum modified to Preschool 2 for individuals who'd benefit
+             from it.  It’s ideal for older 3’s, 4’s & 5 year olds.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div id="stay-and-play-enroll" class="class-section class-5">
+      <div class="container">
+        <div class="class-1-header text-center">
+          <h1>Stay & Play</h1>
+        </div>
+        <div class="class-download pull-right">
+          <h2>Interested in enrolling?</h2>
+          <p>Download the registration form below.</p>
+          <div class="btn btn-primary">Get My Form</div>
+        </div>
+        <div class="class-info">
+          <div class="class-info-header">
+            <h3><i class="fa fa-child"></i> Ages</h3>
+          </div>
+            <div class="class-info-item info-1">
+              <p>4-5 years old (must be 4 by August 1 of the school year)</p>
+            </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-usd"></i> Tuition</h3>
+          </div>
+          <div class="tuition-info">
+            <div class="class-info-item info-1">
+              <p>$125/month for 1 class/week</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>$200/month for 2 classes/week</p>
+            </div>
+          </div>
+          <div class="class-info-header">
+            <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+          </div>
+          <div class="schedule-info">
+            <div class="class-info-item info-1">
+              <p>Monday/Wednesday/Friday 9:00-11:30 AM</p>
+            </div>
+            <div class="class-info-item info-2">
+              <p>Monday-Friday 9:00-11:30 AM</p>
+            </div>
+            <div class="class-info-item info-3">
+              <p>*Tuesday/Wednesday/Thursday 12:30-3:00 PM</p>
+            </div>
+            <div class="class-info-item info-4">
+              <p>**Tuesday-Wednesday-Thursday 12:30-2:50 PM</p>
+            </div>
+          </div>
+        </div>
+        <div class="class-info-notes">
+          <p>
+            *This class is for 3-5yr olds with a Pre-K curriculum modified to Preschool 2 for individuals who'd benefit
+             from it.  It’s ideal for older 3’s, 4’s & 5 year olds.
           </p>
         </div>
       </div>
--- page-templates/page-programs.php
+++ page-templates/page-programs.php
@@ -9,7 +9,7 @@ Template Name: Programs
 <div id="content">
   <div id="programs-page-header">
     <div class="container">
-      <h1 id="program-header" class="text-center text-white">Programs & Rates</h1>
+      <h1 id="program-header" class="text-center text-white">Our Classes & Programs</h1>
       <p id="program-subheader" class="text-center text-white">We offer classes for four main age groups</p>
     </div>
   </div>
@@ -43,14 +43,28 @@ Template Name: Programs
         <div class="class-description">
           <h2>Parent Infant Toddler</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
+            The Parent-Infant-Toddler Program offers classes that parent/caregiver and child attend together one morning
+             each week. This program has a long history at Village Nursery School and First United Methodist; it is one
+             of the few such programs in the city. Each class is led by a teacher and includes an activity time, a
+             snack, circle time and large motor time.
+          </p>
+          <p>
+            During activity time a variety of toys and materials are provided for parent and child to explore together.
+            Examples may be building and transportation toys, shape and color sorters, puzzles, play dough, paint, and
+            a sensory table for sand and water play. There is plenty of time for socializing for parents and children
+            during this time as well.
+          </p>
+          <p>
+            Parents take turns providing a light, healthy snack, and then everyone participates (as they are able) in
+             a short circle time. During circle time, the teacher shares songs and fingerplays then reads a story. Class
+             ends with large motor play in a separate classroom.
+          </p>
+          <p>
+            P-I-T classes meet for 28 weeks during the school year from September through April.  The age-appropriate
+            classes meet from 9:30-11:00AM in Room 214 and 216 at First United Methodist Church.
           </p>
         </div>
-        <a href="<?php echo home_url(); ?>/enrollment/#class-1" class="btn btn-primary">Enroll Now</a>
+        <a href="<?php echo home_url(); ?>/enrollment/#pit-enroll" class="btn btn-primary">Enroll Now</a>
       </div>
     </div>
     <div id="stay-and-play" class="class-section class-3">
@@ -65,7 +79,7 @@ Template Name: Programs
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
-        <a href="<?php echo home_url(); ?>/enrollment/#class-1" class="btn btn-primary">Enroll Now</a>
+        <a href="<?php echo home_url(); ?>/enrollment/#stay-and-play-enroll" class="btn btn-primary">Enroll Now</a>
       </div>
     </div>
   </div>
