d597eea43ac865f51aaf413d4c8a9294832334bd
Kyle Turco <kyleturco@gmail.com>
2017-02-07 00:09:32 -0600

    feat(Enrollment): add 2017 page template and styles

---
+++ page-templates/page-enrollment-2017.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Enrollment 2017
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="enrollment-page-header">
+  <div class="container">
+    <h1 id="enrollment-header" class="text-center text-white">Enrollment 2017-2018</h1>
+    <p id="enrollment-subheader" class="text-center text-white">Important Dates &amp; Info</p>
+  </div>
+</div>
+<div class="enrollment__page-content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 pull-right">
+        <div class="enrollment__alert alert alert-info">
+          <h3 class="enrollment__alert--header">
+            <i class="fa fa-exclamation-triangle icon" aria-hidden="true"></i>
+            Notice:
+          </h3>
+          <p>Enrollment must be completed in person by theparent/guardian of the student.</p>
+          <p>We apologize but no exceptions will be made.</p>
+        </div>
+      </div>
+      <div class="col-md-8 pull-left">
+        <h1 class="enrollment__content-header">Dates to Know</h1>
+        <div class="enrollment__date--container">
+          <h3 class="enrollment__date--date">Monday, March 6th: 5:30-7PM</h3>
+          <h4>Open House</h4>
+          <p class="enrollment__date--description">
+            Monday, March 6th, 5:30-7PM – Come meet our staff, visit our classrooms and learn more information
+            about the multiple programs we offer at Village Nursery School.
+          </p>
+        </div>
+        <div class="enrollment__date--container">
+          <h3 class="enrollment__date--date">Monday, March 20th: 8:30am - 1pm</h3>
+          <h4>ENROLLMENT for VNS Staff, Board &amp; FUMC Church Members Opens</h4>
+        </div>
+        <div class="enrollment__date--container">
+          <h3 class="enrollment__date--date">Wednesday, March 22nd: 8:30am - 3pm</h3>
+          <h4>ENROLLMENT for Current VNS &amp; PIT Families Opens</h4>
+        </div>
+        <div class="enrollment__date--container">
+          <h3 class="enrollment__date--date">Thursday, March 23rd: 8:30am - 2pm</h3>
+          <h4>ENROLLMENT for VNS &amp; PIT past Alumni Opens</h4>
+        </div>
+        <div class="enrollment__date--container">
+          <h3 class="enrollment__date--date">Friday, March 24th: 8:30am - 3pm</h3>
+          <h4>ENROLLMENT to the Public Opens</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
