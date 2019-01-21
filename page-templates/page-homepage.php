a5667fd9215fd0ac4e41a0a07b3f519f6dfde65c
Kyle Turco <kyleturco@gmail.com>
2019-01-20 23:25:59 -0600

    feat(Homepage): update enroll link

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -23,11 +23,11 @@ Template Name: Home Page
     <div class="container">
       <div class="row">
         <div class="col-sm-8">
-          <h2 class="enrollment-cta__header">Enrollment 2018-2019</h2>
+          <h2 class="enrollment-cta__header">Enrollment 2019-2020</h2>
           <p class="enrollment-cta__description h3">Important dates and information</p>
         </div>
         <div class="col-sm-4">
-          <a href="<?php echo home_url(); ?>/enrollment-2018-2019/" class="enrollment-cta__btn btn btn-primary">
+          <a href="<?php echo home_url(); ?>/enrollment-2019-2020/" class="enrollment-cta__btn btn btn-primary" title="Village Nursery School Enrollment Info">
             View Dates <i class="fa fa-angle-double-right"></i>
           </a>
         </div>
