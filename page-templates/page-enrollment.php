8dcd7ff31166d2587b82eaf36ab82e904d93ce11
Kyle Turco <kyleturco@gmail.com>
2022-05-09 22:04:58 -0500

    feat(Enrollment): final enrollment updates

--- page-templates/page-enrollment.php
+++ page-templates/page-enrollment.php
@@ -29,6 +29,26 @@ $pre_k_tuition = get_field('pre_k_tuition');
 $pre_k_scheduling = get_field('pre_k_scheduling');
 $pre_k_additional = get_field('pre_k_additional');
 
+// PreK Variables
+$preschool_spanish_title = get_field('preschool_spanish_title');
+$preschool_spanish_age = get_field('preschool_spanish_age');
+$preschool_spanish_tuition = get_field('preschool_spanish_tuition');
+$preschool_spanish_scheduling = get_field('preschool_spanish_scheduling');
+$preschool_spanish_additional = get_field('preschool_spanish_additional');
+
+// PIT Variables
+$pit_title = get_field('pit_title');
+$pit_age = get_field('pit_age');
+$pit_tuition = get_field('pit_tuition');
+$pit_scheduling = get_field('pit_scheduling');
+$pit_additional = get_field('pit_additional');
+
+// Stay and Play Variables
+$stay_and_play_title = get_field('stay_and_play_title');
+$stay_and_play_age = get_field('stay_and_play_age');
+$stay_and_play_tuition = get_field('stay_and_play_tuition');
+$stay_and_play_scheduling = get_field('stay_and_play_scheduling');
+$stay_and_play_additional = get_field('stay_and_play_additional');
 
 ?>
 
@@ -222,7 +242,7 @@ $pre_k_additional = get_field('pre_k_additional');
     <div id="preschool-spanish" class="class-section class-4">
       <div class="container">
         <div class="class-1-header text-center">
-          <h1>Preschool Spanish</h1>
+          <h1><?php echo $preschool_spanish_title; ?></h1>
         </div>
         <div class="class-download">
           <h2 class="mb-4 mt-0">Interested in enrolling?</h2>
@@ -230,33 +250,42 @@ $pre_k_additional = get_field('pre_k_additional');
           <a href="<?php echo $vns_enrollment_form; ?>" class="btn btn-primary form-download-btn" target="_blank">Get My Form</a>
         </div>
         <div>
-          <div class="class-info-header">
-            <h3><i class="fa fa-child"></i> Ages</h3>
-          </div>
-        <div class="mb-5 class-info">
-          <div class="class-info-item">
-            <p>4-5 years old (must be 4 by August 1 of the school year)</p>
-          </div>
-        </div>
-        <div class="class-info-header">
-          <h3><i class="fa fa-usd"></i> Tuition</h3>
-        </div>
-        <div class="mb-5 class-info">
-          <div class="tuition-info">
-            <div class="class-info-item">
-              <p>$100 month for 2 classes/week</p>
+          <?php if ($preschool_spanish_age): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-child"></i> Ages</h3>
             </div>
-          </div>
-        </div>
-        <div class="class-info-header">
-          <h3><i class="fa fa-calendar"></i> Scheduling</h3>
-        </div>
-        <div class="mb-5 class-info">
-          <div class="schedule-info">
-            <div class="class-info-item">
-              <p>Tuesday/Thursday 9:20-11:50 AM</p>
+            <div class="mb-5 class-info class-info-item">
+              <p><?php echo $preschool_spanish_age; ?></p>
             </div>
+          <?php endif; ?>
+
+          <?php if ($preschool_spanish_tuition): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-usd"></i> Tuition</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $preschool_spanish_tuition; ?>
+            </div>
+          <?php endif; ?>
+
+          <?php if ($preschool_spanish_scheduling): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $preschool_spanish_scheduling; ?>
+            </div>
+          <?php endif; ?>
+        </div>
+
+        <?php if ($preschool_spanish_additional): ?>
+          <div class="class-info-notes">
+            <p>
+              <?php echo $preschool_spanish_additional; ?>
+            </p>
           </div>
+        <?php endif; ?>
+
           <div class="class-download class-download-mobile">
             <h2 class="mb-4 mt-0">Interested in enrolling?</h2>
             <p class="mt-1">Download the registration form below.</p>
@@ -269,7 +298,7 @@ $pre_k_additional = get_field('pre_k_additional');
     <div id="pit-enroll" class="class-section class-5">
       <div class="container">
         <div class="class-1-header text-center">
-          <h1>P.I.T.</h1>
+          <h1><?php echo $pit_title; ?></h1>
         </div>
         <div class="class-download">
           <h2 class="mb-4 mt-0">Interested in enrolling?</h2>
@@ -277,32 +306,42 @@ $pre_k_additional = get_field('pre_k_additional');
           <a href="<?php echo $pit_enrollment_form; ?>" class="btn btn-primary form-download-btn" target="_blank">Get My Form</a>
         </div>
     
-        <div class="class-info-header">
-          <h3><i class="fa fa-child"></i> Ages</h3>
+        <div>
+          <?php if ($pit_age): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-child"></i> Ages</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <p><?php echo $pit_age; ?></p>
+            </div>
+          <?php endif; ?>
+
+          <?php if ($pit_tuition): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-usd"></i> Tuition</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $pit_tuition; ?>
+            </div>
+          <?php endif; ?>
+
+          <?php if ($pit_scheduling): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $pit_scheduling; ?>
+            </div>
+          <?php endif; ?>
         </div>
-        <div class="class-info">
-          <div class="class-info-item">
-            <p>Classes for toddlers 12-24+ months</p>
+
+        <?php if ($pit_additional): ?>
+          <div class="class-info-notes">
+            <p>
+              <?php echo $pit_additional; ?>
+            </p>
           </div>
-        </div>
-    
-        <div class="class-info-header">
-          <h3><i class="fa fa-usd"></i> Tuition</h3>
-        </div>
-        <div class="class-info tuition-info">
-          <div class="class-info-item">
-            <p>$125/semester – paid in full at registration</p>
-          </div>
-        </div>
-    
-        <div class="class-info-header">
-          <h3><i class="fa fa-calendar"></i> Scheduling</h3>
-        </div>
-        <div class="class-info schedule-info">
-          <div class="class-info-item">
-            <p>Classes for students 12-24+ months are held Tuesday-Friday, from 9:30-11:00.</p>
-          </div>
-        </div>
+        <?php endif; ?>
 
         <div class="class-download class-download-mobile">
           <h2 class="mb-4 mt-0">Interested in enrolling?</h2>
@@ -314,7 +353,7 @@ $pre_k_additional = get_field('pre_k_additional');
     <div id="stay-and-play-enroll" class="class-section class-6">
       <div class="container">
         <div class="class-1-header text-center">
-          <h1>Stay & Play</h1>
+          <h1><?php echo $stay_and_play_title; ?></h1>
         </div>
         <div class="class-download">
           <h2 class="mb-4 mt-0">Interested in enrolling?</h2>
@@ -322,35 +361,42 @@ $pre_k_additional = get_field('pre_k_additional');
           <a href="<?php echo $stay_and_play_enrollment_form; ?>" class="btn btn-primary form-download-btn" target="_blank">Get My Form</a>
         </div>
         <div>
-          <div class="class-info-header">
-            <h3><i class="fa fa-child"></i> Ages</h3>
-          </div>
-          <div class="mb-5 class-info">
-            <div class="class-info-item">
-              <p>Must be 12 months old (and walking)</p>
+          <?php if ($stay_and_play_age): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-child"></i> Ages</h3>
             </div>
-          </div>
-          <div class="class-info-header">
-            <h3><i class="fa fa-usd"></i> Tuition</h3>
-          </div>
-          <div class="mb-5 class-info">
-            <div class="tuition-info">
-              <div class="class-info-item">
-                <p>$25 flat rate (Please inquire about before-school care if needed)</p>
-              </div>
+            <div class="mb-5 class-info class-info-item">
+              <p><?php echo $stay_and_play_age; ?></p>
             </div>
-          </div>
-          <div class="class-info-header">
-            <h3><i class="fa fa-calendar"></i> Scheduling</h3>
-          </div>
-          <div class="mb-5 class-info">
-            <div class="schedule-info">
-              <div class="class-info-item">
-                <p>8:45am - 12:00pm</p>
-              </div>
+          <?php endif; ?>
+
+          <?php if ($stay_and_play_tuition): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-usd"></i> Tuition</h3>
             </div>
-          </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $stay_and_play_tuition; ?>
+            </div>
+          <?php endif; ?>
+
+          <?php if ($stay_and_play_scheduling): ?>
+            <div class="class-info-header">
+              <h3><i class="fa fa-calendar"></i> Scheduling</h3>
+            </div>
+            <div class="mb-5 class-info class-info-item">
+              <?php echo $stay_and_play_scheduling; ?>
+            </div>
+          <?php endif; ?>
         </div>
+
+        <?php if ($stay_and_play_additional): ?>
+          <div class="class-info-notes">
+            <p>
+              <?php echo $stay_and_play_additional; ?>
+            </p>
+          </div>
+        <?php endif; ?>
+
       </div>
     </div>
   </div>
